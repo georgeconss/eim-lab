90401ba42d63f6d003664e247eaccf23255513d4
George Constantin <georrctin@gmail.com>
2022-03-14 09:35:17 +0200

    lab 02 done

--- Laborator02/labtasks/ActivityLifecycleMonitor/app/src/main/java/ro/pub/systems/eim/lab02/activitylifecyclemonitor/graphicuserinterface/LifecycleMonitorActivity.java
+++ Laborator02/labtasks/ActivityLifecycleMonitor/app/src/main/java/ro/pub/systems/eim/lab02/activitylifecyclemonitor/graphicuserinterface/LifecycleMonitorActivity.java
@@ -7,6 +7,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.PopupWindow;
 
@@ -66,6 +67,22 @@ public class LifecycleMonitorActivity extends AppCompatActivity {
 
         if (savedInstanceState == null) {
             Log.d(Constants.TAG, "onCreate() method was invoked without a previous state");
+        } else {
+            Log.d(Constants.TAG, "onCreate() method was invoked with a previous state");
+            if (savedInstanceState.containsKey(Constants.USERNAME_EDIT_TEXT)) {
+                EditText usernameEditText = (EditText)findViewById(R.id.username_edit_text);
+                usernameEditText.setText(savedInstanceState.getString(Constants.USERNAME_EDIT_TEXT));
+            }
+
+            if (savedInstanceState.containsKey(Constants.PASSWORD_EDIT_TEXT)) {
+                EditText passwordEditText = (EditText)findViewById(R.id.password_edit_text);
+                passwordEditText.setText(savedInstanceState.getString(Constants.PASSWORD_EDIT_TEXT));
+            }
+
+            if (savedInstanceState.containsKey(Constants.REMEMBER_ME_CHECKBOX)) {
+                CheckBox rememberMeCheckbox = (CheckBox)findViewById(R.id.remember_me_checkbox);
+                rememberMeCheckbox.setChecked(savedInstanceState.getBoolean(Constants.REMEMBER_ME_CHECKBOX));
+            }
         }
     }
 
@@ -109,11 +126,36 @@ public class LifecycleMonitorActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         Log.d(Constants.TAG, "onSaveInstanceState() method was invoked");
+
+        EditText usernameEditText = (EditText)findViewById(R.id.username_edit_text);
+        EditText passwordEditText = (EditText)findViewById(R.id.password_edit_text);
+        CheckBox rememberMeCheckbox = (CheckBox)findViewById(R.id.remember_me_checkbox);
+
+        if (rememberMeCheckbox.isChecked()) {
+            savedInstanceState.putString(Constants.USERNAME_EDIT_TEXT, usernameEditText.getText().toString());
+            savedInstanceState.putString(Constants.PASSWORD_EDIT_TEXT, passwordEditText.getText().toString());
+            savedInstanceState.putBoolean(Constants.REMEMBER_ME_CHECKBOX, rememberMeCheckbox.isChecked());
+        }
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         Log.d(Constants.TAG, "onRestoreInstanceState() method was invoked");
+
+//        if (savedInstanceState.containsKey(Constants.USERNAME_EDIT_TEXT)) {
+//            EditText usernameEditText = (EditText)findViewById(R.id.username_edit_text);
+//            usernameEditText.setText(savedInstanceState.getString(Constants.USERNAME_EDIT_TEXT));
+//        }
+//
+//        if (savedInstanceState.containsKey(Constants.PASSWORD_EDIT_TEXT)) {
+//            EditText passwordEditText = (EditText)findViewById(R.id.password_edit_text);
+//            passwordEditText.setText(savedInstanceState.getString(Constants.PASSWORD_EDIT_TEXT));
+//        }
+//
+//        if (savedInstanceState.containsKey(Constants.REMEMBER_ME_CHECKBOX)) {
+//            CheckBox rememberMeCheckbox = (CheckBox)findViewById(R.id.remember_me_checkbox);
+//            rememberMeCheckbox.setChecked(savedInstanceState.getBoolean(Constants.REMEMBER_ME_CHECKBOX));
+//        }
     }
 }
